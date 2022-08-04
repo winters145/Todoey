7dbb93a557a6e7f5176d6a5077e45258cbd8a7b5
Jack Winterschladen <winterschladen.j@gmail.com>
2022-08-04 11:42:50 +0100

    Add method to add checkmark to pressed todo.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -22,16 +22,29 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
         if #available(iOS 14.0, *) {
             var cellContent = cell.defaultContentConfiguration()
             cellContent.text = itemArray[indexPath.row]
             cell.contentConfiguration = cellContent
+            print(cellContent.text!)
         } else {
             // Fallback on earlier versions
             cell.textLabel?.text = itemArray[indexPath.row]
         }
+        
         return cell
     }
-    
+        
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
