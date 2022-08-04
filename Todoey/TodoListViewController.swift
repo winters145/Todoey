dd9853ffd61b2b94df2f35bd461406fa4a40b92f
Jack Winterschladen <winterschladen.j@gmail.com>
2022-08-04 11:07:14 +0100

    Create tableView methods to display temporary list of items.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["Shopping List", "Work things", "Events"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        if #available(iOS 14.0, *) {
+            var cellContent = cell.defaultContentConfiguration()
+            cellContent.text = itemArray[indexPath.row]
+            cell.contentConfiguration = cellContent
+        } else {
+            // Fallback on earlier versions
+            cell.textLabel?.text = itemArray[indexPath.row]
+        }
+        return cell
+    }
+    
 }
 
