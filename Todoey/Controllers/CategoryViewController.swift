07fb60b59162a3992c18b9afcb00e4455f7a9d6c
Jack Winterschladen <winterschladen.j@gmail.com>
2022-08-05 12:21:18 +0100

    Implement addNewCategories button and saveCategories / loadCategories methods.

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -17,6 +17,7 @@ class CategoryViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadCategories()
     }
 
     //MARK: - TableView Datasource Methods
@@ -42,18 +43,56 @@ class CategoryViewController: UITableViewController {
         
         return cell
     }
-    //MARK: - Data Manipulation Methods
-    
     
     //MARK: - Add New Categories
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Category", message: "", preferredStyle: .alert)
+
+        let action = UIAlertAction(title: "Add Category", style: .default) { action in
+            
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            
+            self.categoryArray.append(newCategory)
+            self.saveCategories()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new category"
+            textField = alertTextField
+        }
+    
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
     
+    //MARK: - Data Manipulation Methods
     
+    func saveCategories() {
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context, \(error)")
+        }
+        
+        tableView.reloadData()
+    }
     
-    
-    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        
+        do {
+            categoryArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
+    }
     //MARK: - TableView Delegate Methods
     
 }
